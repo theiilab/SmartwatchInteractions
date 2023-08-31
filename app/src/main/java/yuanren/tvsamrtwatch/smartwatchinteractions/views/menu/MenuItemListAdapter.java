32c357ec99d45f32767f6a10bf8101c21b397d52
EowynREN <eowynren@163.com>
2023-08-31 11:51:38 -0700

    change channel via menu (still with broken pipe error)

--- app/src/main/java/yuanren/tvsamrtwatch/smartwatchinteractions/views/menu/MenuItemListAdapter.java
+++ app/src/main/java/yuanren/tvsamrtwatch/smartwatchinteractions/views/menu/MenuItemListAdapter.java
@@ -1,15 +1,23 @@
 package yuanren.tvsamrtwatch.smartwatchinteractions.views.menu;
 
+import android.os.AsyncTask;
+import android.os.Build;
+import android.util.Log;
+import android.view.KeyEvent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.RequiresApi;
 import androidx.wear.widget.WearableRecyclerView;
 
 import yuanren.tvsamrtwatch.smartwatchinteractions.R;
+import yuanren.tvsamrtwatch.smartwatchinteractions.utils.NetworkUtils;
 
 public class MenuItemListAdapter extends WearableRecyclerView.Adapter {
+    public static final String TAG = "MenuItemListAdapter";
+    public static int currentSelectedMenuItem = 1;
     private int[] icons;
 
     private String[] names;
@@ -32,10 +40,48 @@ public class MenuItemListAdapter extends WearableRecyclerView.Adapter {
         MenuItemViewHolder menuItemViewHolder = (MenuItemViewHolder) holder;
         menuItemViewHolder.icon.setImageDrawable(holder.itemView.getContext().getDrawable(icons[position]));
         menuItemViewHolder.name.setText(names[position]);
+        menuItemViewHolder.menuItem.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int diff = menuItemViewHolder.getLayoutPosition() - currentSelectedMenuItem;
+                performActionBy(diff);
+                new SocketAsyncTask().execute(KeyEvent.KEYCODE_DPAD_CENTER);
+                currentSelectedMenuItem = menuItemViewHolder.getLayoutPosition();
+            }
+        });
     }
 
     @Override
     public int getItemCount() {
         return icons.length;
+    }
+
+    private void performActionBy(int diff) {
+        if (diff == 0) { return; }
+        int count = diff < 0 ? diff * -1 : diff;
+
+        if (diff < 0) {
+            for (int i = 0; i < count; ++i) {
+                new SocketAsyncTask().execute(KeyEvent.KEYCODE_DPAD_UP);
+            }
+        } else {
+            for (int i = 0; i < count; ++i) {
+                new SocketAsyncTask().execute(KeyEvent.KEYCODE_DPAD_DOWN);
+            }
+        }
+    }
+
+    private class SocketAsyncTask extends AsyncTask<Integer, String, Void> {
+        @RequiresApi(api = Build.VERSION_CODES.TIRAMISU)
+        @Override
+        protected Void doInBackground(Integer... integers) {
+            NetworkUtils.sendCommand(integers[0]);
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(Void unused) {
+            super.onPostExecute(unused);
+        }
     }
 }
--- app/src/main/java/yuanren/tvsamrtwatch/smartwatchinteractions/views/menu/MenuItemViewHolder.java
+++ app/src/main/java/yuanren/tvsamrtwatch/smartwatchinteractions/views/menu/MenuItemViewHolder.java
@@ -5,17 +5,19 @@ import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
+import androidx.constraintlayout.widget.ConstraintLayout;
 import androidx.wear.widget.WearableRecyclerView;
 
 import yuanren.tvsamrtwatch.smartwatchinteractions.R;
 
 public class MenuItemViewHolder extends WearableRecyclerView.ViewHolder {
+    ConstraintLayout menuItem;
     ImageView icon;
     TextView name;
 
     public MenuItemViewHolder(@NonNull View itemView) {
         super(itemView);
-
+        menuItem = itemView.findViewById(R.id.menu_item);
         icon = itemView.findViewById(R.id.icon);
         name = itemView.findViewById(R.id.name);
     }
